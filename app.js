2a6f87a60f04b1e949a39e89197230394c10bd76
Zeeshan Ahmed <114758827+zeeshi1233@users.noreply.github.com>
2023-05-21 00:02:08 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+function _(selector) {
+    return document.querySelector(selector);
+  }
+  
+  function setup() {
+    let canvas = createCanvas(650, 600);
+    canvas.parent("canvas-wrapper");
+    background(255);
+  }
+  
+  function mouseDragged() {
+    let type = _("#pen-pencil").checked ? "pencil" : "brush";
+    let size = parseInt(_("#pen-size").value);
+    let color = _("#pen-color").value;
+  
+    fill(color);
+    stroke(color);
+  
+    if (type == "pencil") {
+      line(pmouseX, pmouseY, mouseX, mouseY);
+    } else {
+      ellipse(mouseX, mouseY, size, size);
+    }
+  }
+  
